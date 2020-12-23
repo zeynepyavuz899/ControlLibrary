bbcf2ef65e8d281c88d8c05e73ea651e39a91362
Zeynep Yavuz <yavuz.zeynep@std.izu.edu.tr>
2020-12-23 18:55:16 +0300

    login

--- libraryManagement/Frmlogadmin.cs
+++ libraryManagement/Frmlogadmin.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace libraryManagement
 {
@@ -16,10 +17,46 @@ namespace libraryManagement
         {
             InitializeComponent();
         }
+        sqlbaglantisi bgl = new sqlbaglantisi();
 
         private void loginbtn_Click(object sender, EventArgs e)
         {
-            
+            SqlCommand komut = new SqlCommand("Select * from  Tbl_Librarian where LibrarianTC=@k1 and LibrarianPassword=@k2", bgl.baglanti());
+            komut.Parameters.AddWithValue("@k1", idtb.Text);
+            komut.Parameters.AddWithValue("@k2", passtb.Text);
+            SqlDataReader dr = komut.ExecuteReader();
+            string id = idtb.Text.Trim();
+            string pass = passtb.Text.Trim();
+
+            if(dr.Read ())
+            {
+                Form1 fr = new Form1();
+                fr.Show();
+                this.Hide();
+
+            }
+
+            else
+            {
+                if(String.IsNullOrEmpty(id))
+                {
+                    MessageBox.Show("please enter your ID");
+                    idtb.Focus();
+                    return;
+                }
+                if(String.IsNullOrEmpty(pass))
+                {
+                    MessageBox.Show("please enter your PASSWORD");
+                    passtb.Focus();
+                    return;
+                }
+                else
+                {
+                    MessageBox.Show("ID or PASSWORD is wrong");
+                }
+                
+            }
+            bgl.baglanti().Close();
         }
     }
 }
